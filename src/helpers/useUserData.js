1fd0f77e5b15fda2b48371dd2a6955165509eac1
DmytroOvchar <ov4ar.dmytro@gmail.com>
2024-02-16 14:58:01 +0200

    devTools

--- src/helpers/useUserData.js
+++ src/helpers/useUserData.js
@@ -1,12 +1,13 @@
 import { useQuery } from "react-query";
 
-export const useUserData = ( userId ) => {
+export const useUserData = (userId) => {
   const userData = useQuery({
     queryKey: ["users", userId],
     queryFn: async () => {
       const res = await fetch(`/api/users/${userId}`);
       return res.json();
     },
+    staleTime: 1000 * 60 * 5,
   });
 
   return userData;
